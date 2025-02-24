d62395fac4a4b56f6c6d8161ad1eb3548c682d04
salvoparisi <psalvo065@gmail.com>
2025-02-24 20:35:49 +0100

    add getChefBirthday

---
+++ main.js
@@ -0,0 +1,27 @@
+async function fetchJson(url) {
+    const res = await fetch(url)
+    const obj = await res.json()
+    return obj
+}
+
+async function getChefBirthday(id) {
+    let recipes;
+    try {
+        recipes = await fetchJson(`https://dummyjson.com/recipes/${id}`)
+    } catch {
+        throw new Error("Ricetta non Trovata")
+    }
+    try {
+        let userId = recipes.userId
+        chef = await fetchJson(`https://dummyjson.com/users/${userId}`)
+    } catch {
+        throw new Error("Chef non Trovato")
+    }
+    return chef.birthDate;
+}
+
+
+
+getChefBirthday(1)
+    .then(birthDate => console.log(`La data di nascita dello chef è ${birthDate}`))
+    .catch(error => console.error("Errore:", error.message))
